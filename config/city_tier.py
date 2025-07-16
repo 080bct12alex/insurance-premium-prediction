3bec7340011d4150fd95d99f3dbebd006d20d680
Alex Shrestha <080bct012.alex@pcampus.edu.np>
2025-07-16 10:17:24 +0545

    first commit

---
+++ config/city_tier.py
@@ -0,0 +1,18 @@
+tier_1_cities_nepal = [
+    "Kathmandu", "Pokhara", "Lalitpur", "Bhaktapur", "Biratnagar", "Birgunj", "Bharatpur"
+]
+
+tier_2_cities_nepal = [
+    "Dharan", "Janakpur", "Butwal", "Hetauda", "Nepalgunj", "Dhangadhi",
+    "Itahari", "Bhairahawa", "Tulsipur", "Gaur", "Inaruwa", "Rajbiraj", "Ilam",
+    "Damak", "Bardibas", "Kalaiya", "Banepa", "Dipayal", "Lahan", "Bhadrapur",
+    "Kakarbhitta", "Parasi", "Gulariya", "Kohalpur", "Kapilvastu", "Birtamode"
+]
+
+tier_3_cities_nepal = [
+    "Tikapur", "Baglung", "Tansen", "Gorkha", "Beni", "Khotang", "Jaleshwar",
+    "Dhankuta", "Sindhuli", "Phidim", "Panauti", "Melamchi", "Khandbari",
+    "Waling", "Amargadhi", "Resunga", "Besisahar", "Chainpur", "Bayalpata",
+    "Okhaldhunga", "Sandhikharka", "Darchula", "Manma", "Salleri", "Damakha",
+    "Rukumkot", "Jumla", "Martadi", "Triveni", "Putalibazar"
+]
